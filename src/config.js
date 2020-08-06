a5c448f695f2173f5063e68b2d841deb118142bf
Raza Hussain <40444972+razahuss@users.noreply.github.com>
2020-08-05 20:44:30 -0400

    Setting up React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads111"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://1qfriw0z43.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_6Uniucd8R",
+    APP_CLIENT_ID: "7bkgbkg819c7hd15qipsohgps8",
+    IDENTITY_POOL_ID: "us-east-1:8dbe3c3d-0bd1-4fce-95e0-0849e13dcad7"
+  }
+};
