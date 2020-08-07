7b2d3fd1c6ea6a050606686e0c8ad0ce3cfa1c39
Raza Hussain <40444972+razahuss@users.noreply.github.com>
2020-08-06 22:06:59 -0400

    Building React App

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+	MAX_ATTACHMENT_SIZE: 5000000,
+	STRIPE_KEY: "pk_test_51HCVFAATMehWBxjmIeZZ5ypjyh7qbnUmaFPatBVmWULxmcJwk3y9m3cFqUbFkDjsemJw7arqSVpWJDoETfRBaJQI00k0SUIPUL",
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-uploads111"
